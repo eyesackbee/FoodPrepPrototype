cbbdb45b55b2ebf5d5cc13408c2e3e99f2818239
eyesackbee <isaacmbewley@gmail.com>
2024-12-03 12:59:06 +0000

    03/12 build

--- Assets/clickplace.cs
+++ Assets/clickplace.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class clickplace : MonoBehaviour
 {
+    public serveplate plate;
+
     public int foodValue;
     static float offset = 0;
 
     private void OnMouseDown()
     {
         Transform clone = Instantiate(transform);
+        plate.AddFood(clone);
         clone.localScale = Vector3.one * 0.5f;
         clone.localPosition = new Vector3(4, 3 + offset, - offset);
         Debug.Log("cloned");
         offset += 0.1f;
-            
+
+        gameflow.plateValue += foodValue;
     }
 }
